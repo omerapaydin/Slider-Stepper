4edd7df748153b8b11b941c88fbe56e332d8e44e
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 22:44:53 +0300

    First

--- Slider-Stepper/ViewController.swift
+++ Slider-Stepper/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderDegis(_ sender: Any) {
+        
+        sliderLabel.text = "\(Int(slider.value))"
+        
+    }
+    
+    @IBAction func stepperDegis(_ sender: Any) {
+        
+        stepperLabel.text = "\(Int(stepper.value))"
+        
+    }
+    
+    @IBAction func tikla(_ sender: Any) {
+        
+        print("Slider değer : \(Int(slider.value))")
+        print("Stepper değer : \(Int(stepper.value))")
+    }
+    
 }
 
