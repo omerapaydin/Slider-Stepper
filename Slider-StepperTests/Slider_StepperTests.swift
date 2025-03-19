892fdfb597a6149a8c41449d00b8c3d6806683ef
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 22:20:59 +0300

    Initial Commit

---
+++ Slider-StepperTests/Slider_StepperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_StepperTests.swift
+//  Slider-StepperTests
+//
+//  Created by Ömer on 19.03.2025.
+//
+
+import Testing
+@testable import Slider_Stepper
+
+struct Slider_StepperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
